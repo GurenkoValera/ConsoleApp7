8561106840bb9f154533b4fdb31a72a3207e77c1
valer <valer@DESKTOP-QD94NVR>
2024-10-06 17:45:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод трехзначного числа
+        Console.Write("Введите трехзначное число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        // Проверка, является ли число трехзначным
+        if (number < 100 || number > 999)
+        {
+            Console.WriteLine("Число должно быть трехзначным.");
+            return;
+        }
+
+        // Вычисление обратного порядка числа
+        int reversed = 0;
+        while (number > 0)
+        {
+            reversed = reversed * 10 + number % 10;
+            number /= 10;
+        }
+
+        // Вывод результата
+        Console.WriteLine("Обратный порядок числа: " + reversed);
+    }
+}
